92fc02d4bcda604394a81859f7b0074a3adc312f
Nitin Kumar <nitin241196@gmail.com>
2017-10-27 20:00:14 +0530

    component using props

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 //create Component
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
 
-const element = ( <h1>Hi there</h1> );
+const element = <Welcome name="Nitin" />;
 
-//put component into html page
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+  //put component into html page
+
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
